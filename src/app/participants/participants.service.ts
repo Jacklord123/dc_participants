462293c653a58973f4fd41762888041c78880415
Jacklord123 <konfidentz12@gmail.com>
2021-10-01 16:00:12 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,15 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name: "Don Julio", age:67 + " years old", gender: "Male"},
+            {name: "Pablo Tasyo", age:77 + " years old", gender: "Male"},
+            {name: "Analyn Segovia", age:22 + " years old", gender: "Female"},
+            {name: "Sofia Mason", age:37 + " years old", gender: "Female"},
+            {name: "Nanding Bartoleme", age:55 + " years old", gender: "Male"},
+            {name: "Edmond Dante", age:38 + " years old", gender: "Male"},
+            {name: "Dhain Noel Aguines", age:21 + " years old", gender: "Male"},
+            {name: "Danica Balbero", age:44 + " years old", gender: "Female"},
+        ];
+    }
+
+}
